21861d321d31b871d4ed18976bd17b69df8a1798
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-06-23 10:45:24 +0700

    create Routes for routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
