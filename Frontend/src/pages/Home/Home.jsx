9a6061d43e4c0d2ec8b7714d30efbecbd545be44
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-06-24 15:20:46 +0700

    create the Home page

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Searchbar from "../../components/Searchbar/Searchbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Searchbar />
+    </div>
+  );
 };
 
 export default Home;
