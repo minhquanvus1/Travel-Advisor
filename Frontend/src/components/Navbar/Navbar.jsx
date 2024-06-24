81e7481eeaa41a57b4cf2329d6dbfb05979e0c7b
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-06-24 19:16:24 +0700

    dynamically adjust the Navbar on Scroll

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -20,16 +20,27 @@ const Navbar = () => {
   }, []);
   return (
     <div className={`navbar ${scroll}`}>
-      <a href="/">
-        <img src={assets.logo} alt="logo image" className="logo" />
-      </a>
-      <ul>
-        <li>Home</li>
-        <li>Discover</li>
-        <li>Travel Tips</li>
-        {scroll !== "new-navbar" && <li>More</li>}
-      </ul>
-      <button>Sign in</button>
+      <div className={`row-1 ${scroll}`}>
+        <a href="/">
+          <img src={assets.logo} alt="logo image" className="logo" />
+        </a>
+        {scroll === "new-navbar" && (
+          <div className="search-bar-container">
+            <div className="row">
+              <i className="fa-solid fa-magnifying-glass"></i>
+              <input type="text" placeholder="Search..." />
+            </div>
+          </div>
+        )}
+
+        <ul>
+          <li>Home</li>
+          <li>Discover</li>
+          <li>Travel Tips</li>
+          {scroll !== "new-navbar" && <li>More</li>}
+        </ul>
+        <button>Sign in</button>
+      </div>
     </div>
   );
 };
