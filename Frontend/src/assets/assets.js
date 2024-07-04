45daeeb8527304ecae93778ea1dfad2f82d5f71b
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-04 08:18:57 +0700

    create Cities data in assets.js

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,5 +1,20 @@
 import logo from "./logo.png";
 
 export const assets = {
-    logo
+  logo,
 };
+
+export const cities = [
+  {
+    id: 1,
+    name: "Ho Chi Minh city",
+    imageUrl:
+      "https://vietnamnomad.com/wp-content/uploads/2021/02/Downtown-Ho-Chi-Minh-City-Video.jpg",
+  },
+  {
+    id: 2,
+    name: "Quy Nhon city",
+    imageUrl:
+      "https://vietnamtour.com/images/photos/asia/quy-nhon-city-tour-3.jpg?t=1593696405",
+  },
+];
