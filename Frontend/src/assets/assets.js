4170966e07c4ed762fe98d33b3df71ac9e334983
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-04 13:37:10 +0700

    add the description for each city

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -10,11 +10,15 @@ export const cities = [
     name: "Ho Chi Minh city",
     imageUrl:
       "https://vietnamnomad.com/wp-content/uploads/2021/02/Downtown-Ho-Chi-Minh-City-Video.jpg",
+    description:
+      "Ho Chi Minh City, the pièce de résistance of Vietnam, is a bustling metropolis in no short supply of charm and allure. This vibrant city seamlessly melds historical elegance with contemporary dynamism. Wander through its boulevards and marvel at the stunning architecture, or delve into the culinary scene, where street food and fine dining coexist harmoniously. The historic Hoi Truong Thong Nhat Palace and the intricately adorned Emperor Jade Pagoda stand as testaments to the city’s rich heritage. In Ho Chi Minh City, each corner reveals a new adventure, promising unforgettable experiences at every turn.",
   },
   {
     id: 2,
     name: "Quy Nhon city",
     imageUrl:
       "https://vietnamtour.com/images/photos/asia/quy-nhon-city-tour-3.jpg?t=1593696405",
+    description:
+      "Quy Nhon City, nestled along Vietnam's exquisite coastline, unveils itself as a sanctuary of tranquility and cultural splendor. This coastal haven beckons with its unspoiled beaches, where azure waters gently lap against powdery sands, creating a serene backdrop for contemplation and rejuvenation. Explore the labyrinthine alleys adorned with traditional architecture, offering glimpses of ancient Cham relics and ornate Buddhist sanctuaries that whisper tales of antiquity. Delight in the region's gastronomic treasures, where flavors meld in a symphony of culinary finesse. Quy Nhon epitomizes elegance and allure, inviting discerning travelers to uncover its hidden marvels and indulge in moments of profound discovery.",
   },
 ];
