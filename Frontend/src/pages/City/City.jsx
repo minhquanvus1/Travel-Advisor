36fb38fe3c6a703a33147f82e17495c1d9b9154c
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-04 13:37:46 +0700

    create the City page with image and description of that City

--- Frontend/src/pages/City/City.jsx
+++ Frontend/src/pages/City/City.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./City.css";
 import { useParams } from "react-router-dom";
+import { cities } from "../../assets/assets";
+import ExpandableDescription from "../../components/ExpandableDescription/ExpandableDescription";
+
 const City = () => {
   const { cityName } = useParams();
+  const [city, setCity] = useState(null);
   console.log("cityName is", cityName);
-  return <div className="city">City {cityName}</div>;
+  const findCityByName = () => {
+    const foundCity = cities.find((city) => {
+      console.log("city.name is", city.name);
+      return city.name === cityName;
+    });
+    if (!foundCity) {
+      console.log("No city found");
+      return;
+    }
+    console.log("city in function is", foundCity);
+    setCity(foundCity);
+    return foundCity;
+  };
+  useEffect(() => {
+    findCityByName();
+  }, []);
+  return (
+    <div className="city">
+      {!city && "No city found"}
+      City {cityName}
+      {city && (
+        <>
+          <div className="city-header">
+            <img src={city && city.imageUrl} alt="city header" />
+          </div>
+          <ExpandableDescription
+            title={city.name}
+            text={city.description}
+          ></ExpandableDescription>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default City;
