97c1020d4554f08f630044aac9a698badd7bf435
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-04 14:49:15 +0700

    add the cuisine list to each city

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -12,6 +12,32 @@ export const cities = [
       "https://vietnamnomad.com/wp-content/uploads/2021/02/Downtown-Ho-Chi-Minh-City-Video.jpg",
     description:
       "Ho Chi Minh City, the pièce de résistance of Vietnam, is a bustling metropolis in no short supply of charm and allure. This vibrant city seamlessly melds historical elegance with contemporary dynamism. Wander through its boulevards and marvel at the stunning architecture, or delve into the culinary scene, where street food and fine dining coexist harmoniously. The historic Hoi Truong Thong Nhat Palace and the intricately adorned Emperor Jade Pagoda stand as testaments to the city’s rich heritage. In Ho Chi Minh City, each corner reveals a new adventure, promising unforgettable experiences at every turn.",
+    cuisine: [
+      {
+        id: 1,
+        name: "Pho",
+        imageUrl:
+          "https://asianwaytravel.com/wp-content/uploads/2022/07/ph%E1%BB%9F-b%C3%B2.jpg",
+        description:
+          "Aromatic broth simmered for hours, tender beef slices, and fresh herbs combine to create Vietnam's quintessential noodle soup.",
+      },
+      {
+        id: 2,
+        name: "Banh Mi",
+        imageUrl:
+          "https://tse3.mm.bing.net/th?id=OIP.j_0FHAuCbmn9sxjfZZ0ZOgHaE7&pid=Api&P=0&h=220",
+        description:
+          "Crispy baguette filled with savory pork, pâté, fresh vegetables, and zesty pickles, creating a harmonious symphony of flavors.",
+      },
+      {
+        id: 3,
+        name: "Com Tam",
+        imageUrl:
+          "https://tse1.mm.bing.net/th?id=OIP.srovx59ow2asbG4fNCcb1QHaE7&pid=Api&P=0&h=220",
+        description:
+          "Fragrant broken rice served with succulent grilled pork, a perfectly fried egg, and accompanied by a medley of pickled vegetables and fish sauce.",
+      },
+    ],
   },
   {
     id: 2,
@@ -20,5 +46,31 @@ export const cities = [
       "https://vietnamtour.com/images/photos/asia/quy-nhon-city-tour-3.jpg?t=1593696405",
     description:
       "Quy Nhon City, nestled along Vietnam's exquisite coastline, unveils itself as a sanctuary of tranquility and cultural splendor. This coastal haven beckons with its unspoiled beaches, where azure waters gently lap against powdery sands, creating a serene backdrop for contemplation and rejuvenation. Explore the labyrinthine alleys adorned with traditional architecture, offering glimpses of ancient Cham relics and ornate Buddhist sanctuaries that whisper tales of antiquity. Delight in the region's gastronomic treasures, where flavors meld in a symphony of culinary finesse. Quy Nhon epitomizes elegance and allure, inviting discerning travelers to uncover its hidden marvels and indulge in moments of profound discovery.",
+    cuisine: [
+      {
+        id: 4,
+        name: "Banh Xeo Tom Nhay",
+        imageUrl:
+          "https://tiemchachica.com/wp-content/uploads/2023/03/banh-xeo-binh-dinh-1.jpg",
+        description:
+          "Golden, crispy pancakes filled with succulent shrimp and bean sprouts, served with fresh greens and a tangy dipping sauce.",
+      },
+      {
+        id: 5,
+        name: "Banh It La Gai",
+        imageUrl:
+          "https://tourdanangcity.vn/wp-content/uploads/2023/05/banh-it-la-gai-quy-nhon-binh-dinh-02.jpg",
+        description:
+          "Delicate glutinous rice cakes enveloped in a rich, sweet mung bean filling, wrapped in fragrant banana leaves.",
+      },
+      {
+        id: 6,
+        name: "Seafood",
+        imageUrl:
+          "https://tse1.explicit.bing.net/th?id=OIP.ESel2W-6B_iFAcf4h_tPMwHaE8&pid=Api&P=0&h=220",
+        description:
+          "An array of fresh seafood, from succulent grilled prawns to tender squid, each dish bursting with the flavors of the sea.",
+      },
+    ],
   },
 ];
