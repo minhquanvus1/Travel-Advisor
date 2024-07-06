b8c561f4e855dd170eba967d64c64e6e96beece1
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-06 22:31:25 +0700

    create ToursHeader component, and add it to Tours page

---
+++ Frontend/src/components/ToursHeader/ToursHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ToursHeader.css";
+
+const ToursHeader = () => {
+  return <div className="tours-header"></div>;
+};
+
+export default ToursHeader;
--- Frontend/src/pages/Tours/Tours.jsx
+++ Frontend/src/pages/Tours/Tours.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Tours.css";
 import ToursAdvantageBanner from "../../components/ToursAdvantageBanner/ToursAdvantageBanner";
+import ToursHeader from "../../components/ToursHeader/ToursHeader";
 
 const Tours = () => {
   return (
     <div className="tours-section">
+      <ToursHeader></ToursHeader>
       <div className="tours">
         <ToursAdvantageBanner></ToursAdvantageBanner>
       </div>
