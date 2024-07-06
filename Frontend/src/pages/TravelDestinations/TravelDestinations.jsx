5e6e8fa199b41b9763e0cb2e4d60cc3ffaadfaa5
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-06 15:32:55 +0700

    create the TravelDestinationsService component, and use it in TravelDestinations page

--- Frontend/src/pages/TravelDestinations/TravelDestinations.jsx
+++ Frontend/src/pages/TravelDestinations/TravelDestinations.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./TravelDestinations.css";
 import TravelDestinationsHeader from "../../components/TravelDestinationsHeader/TravelDestinationsHeader";
+import TravelDestinationsService from "../../components/TravelDestinationsService/TravelDestinationsService";
 
 const TravelDestinations = () => {
   return (
     <div className="travel-destinations-section">
       <TravelDestinationsHeader></TravelDestinationsHeader>
-      <div className="travel-destinations">rr</div>
+      <div className="travel-destinations">
+        <TravelDestinationsService></TravelDestinationsService>
+      </div>
     </div>
   );
 };
