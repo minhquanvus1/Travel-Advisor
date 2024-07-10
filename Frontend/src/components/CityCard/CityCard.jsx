0e08e8035c4381ef25057ddd923792cd8755c878
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-10 08:57:10 +0700

    modify the cityName in the Rote Pathname to use underscore if cityName contains whitespace

--- Frontend/src/components/CityCard/CityCard.jsx
+++ Frontend/src/components/CityCard/CityCard.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./CityCard.css";
 import { Link } from "react-router-dom";
+import { CityContext } from "../../context/CityContextProvider";
+import { replaceWhiteSpaceWithUnderScore } from "../../functions/replaceWhiteSpaceWithUnderScore";
 
 const CityCard = ({ city }) => {
+  const { checkAndSetCityState } = useContext(CityContext);
+
   return (
-    <Link to={`/cities/${city.name}`} className="city-link">
+    <Link
+      to={`/cities/${replaceWhiteSpaceWithUnderScore(city.name)}`}
+      className="city-link"
+      onClick={() => checkAndSetCityState(city.name)}
+    >
       <div className="city-card">
         <img src={city.imageUrl} alt="city image" />
         <h2>{city.name}</h2>
