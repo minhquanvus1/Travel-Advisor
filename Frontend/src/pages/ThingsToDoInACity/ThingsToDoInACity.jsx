e080ec145dc70de2c107e1488782ba49ad123ec3
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-07-25 20:47:18 +0700

    display the list of TravelDestinationBigCard components

--- Frontend/src/pages/ThingsToDoInACity/ThingsToDoInACity.jsx
+++ Frontend/src/pages/ThingsToDoInACity/ThingsToDoInACity.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ThingsToDoInACity.css";
 import ThingsToDoInACityHeader from "../../components/ThingsToDoInACityHeader/ThingsToDoInACityHeader";
 import { Link } from "react-router-dom";
 import TravelDestinationBigCard from "../../components/TravelDestinationBigCard/TravelDestinationBigCard";
+import { CityContext } from "../../context/CityContextProvider";
+import { cities, subCategory, attractions } from "../../assets/assets";
 
 const ThingsToDoInACity = () => {
+  const { cityState } = useContext(CityContext);
+  const findAttractionsInThisCity = () => {
+    if (!cityState) return;
+    const foundCity = cities.find((city) => city.name === cityState);
+    console.log("foundCity in thingstodoinacity is", foundCity);
+    const allAttractionsInThisCity = attractions.filter(
+      (attraction) => attraction.cityId === foundCity.id
+    );
+    if (allAttractionsInThisCity.length === 0) {
+      console.log("this city has no attractions");
+      return;
+    }
+    console.log("all attractions are", allAttractionsInThisCity);
+    return allAttractionsInThisCity;
+  };
+  const allAttractionsInThisCity = findAttractionsInThisCity();
   return (
     <div className="things-to-do-in-a-city-section">
       <div className="things-to-do-in-a-city">
@@ -20,16 +38,18 @@ const ThingsToDoInACity = () => {
             </Link>
           </div>
           <div className="top-attractions-list">
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
-            <TravelDestinationBigCard></TravelDestinationBigCard>
+            {!allAttractionsInThisCity &&
+              `City ${cityState} has no attractions`}
+            {allAttractionsInThisCity &&
+              allAttractionsInThisCity.map((attraction, index) => {
+                return (
+                  <TravelDestinationBigCard
+                    key={attraction.id}
+                    index={index}
+                    attraction={attraction}
+                  ></TravelDestinationBigCard>
+                );
+              })}
           </div>
           <Link to="/" className="attractions-link">
             See all
