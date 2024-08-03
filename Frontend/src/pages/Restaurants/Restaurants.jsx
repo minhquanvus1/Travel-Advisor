e7ffdc0d78b25dffd91542cb8843fd9814c05048
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-03 21:03:55 +0700

    create the Restaurants in top destinations section in the Restaurants page

--- Frontend/src/pages/Restaurants/Restaurants.jsx
+++ Frontend/src/pages/Restaurants/Restaurants.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Restaurants.css";
 import RestaurantsHeader from "../../components/RestaurantsHeader/RestaurantsHeader";
 import RestaurantsService from "../../components/RestaurantsService/RestaurantsService";
+import { Link } from "react-router-dom";
+import { cities } from "../../assets/assets";
+import CityCard from "../../components/CityCard/CityCard";
+import { replaceWhiteSpaceWithUnderScore } from "../../functions/replaceWhiteSpaceWithUnderScore";
 
 const Restaurants = () => {
   return (
@@ -10,6 +14,27 @@ const Restaurants = () => {
       <div className="restaurants">
         <div className="restaurants-service-container">
           <RestaurantsService></RestaurantsService>
+        </div>
+        <div className="restaurants-in-top-cities-container">
+          <div className="restaurants-in-top-cities-header">
+            <h3 className="title">Restaurants in top destinations</h3>
+            <a href="#">See all</a>
+          </div>
+          <div className="restaurants-in-top-cities-list">
+            {cities.length > 0 &&
+              cities.map((city, index) => {
+                return (
+                  <Link
+                    to={`/cities/${replaceWhiteSpaceWithUnderScore(
+                      city.name
+                    )}/restaurants`}
+                    key={city.id}
+                  >
+                    <CityCard city={city}></CityCard>
+                  </Link>
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
