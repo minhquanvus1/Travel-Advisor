33724f7e07d80403098de3055f6917d52abbcf52
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-04 11:22:24 +0700

    create the top destinations section in ThingsToDo page

--- Frontend/src/pages/ThingsToDo/ThingsToDo.jsx
+++ Frontend/src/pages/ThingsToDo/ThingsToDo.jsx
@@ -2,13 +2,37 @@ import React from "react";
 import "./ThingsToDo.css";
 import TravelDestinationsHeader from "../../components/TravelDestinationsHeader/TravelDestinationsHeader";
 import TravelDestinationsService from "../../components/TravelDestinationsService/TravelDestinationsService";
+import { cities } from "../../assets/assets";
+import { Link } from "react-router-dom";
+import CityCard from "../../components/CityCard/CityCard";
+import { replaceWhiteSpaceWithUnderScore } from "../../functions/replaceWhiteSpaceWithUnderScore";
 
 const ThingsToDo = () => {
   return (
-    <div className="travel-destinations-section">
+    <div className="things-to-do-section">
       <TravelDestinationsHeader></TravelDestinationsHeader>
-      <div className="travel-destinations">
+      <div className="things-to-do">
         <TravelDestinationsService></TravelDestinationsService>
+        <div className="top-destinations-section">
+          <div className="top-destinations-container">
+            <div className="title">Top destinations in Vietnam</div>
+            <div className="restaurants-in-top-cities-list">
+              {cities.length > 0 &&
+                cities.map((city, index) => {
+                  return (
+                    <Link
+                      to={`/cities/${replaceWhiteSpaceWithUnderScore(
+                        city.name
+                      )}/things-to-do`}
+                      key={city.id}
+                    >
+                      <CityCard city={city}></CityCard>
+                    </Link>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
