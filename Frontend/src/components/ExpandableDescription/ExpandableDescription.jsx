8b53be833280be00e9846153bd5e69d3ea230c3b
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-08 07:51:57 +0700

    update the logic when to show the readmore/readless button

--- Frontend/src/components/ExpandableDescription/ExpandableDescription.jsx
+++ Frontend/src/components/ExpandableDescription/ExpandableDescription.jsx
@@ -1,15 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./ExpandableDescription.css";
 
 const ExpandableDescription = ({ text, lineClamp = 3 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [showReadMoreButton, setShowReadMoreButton] = useState(false);
+  const descriptionRef = useRef(null);
+  // useEffect(() => {
+  //   setShowReadMoreButton(text.length > 100);
+  // }, []);
+
   useEffect(() => {
-    setShowReadMoreButton(text.length > 100);
-  }, []);
+    const descriptionElement = descriptionRef.current;
+    const checkOverflow = () => {
+      if (descriptionElement) {
+        const isOverflowing =
+          descriptionElement.scrollHeight > descriptionElement.clientHeight;
+        console.log("isOverflowing is", isOverflowing);
+        console.log("window innerwidth", window.innerWidth);
+        console.log(
+          "document clientwidth",
+          document.documentElement.clientWidth
+        );
+        setShowReadMoreButton(() => {
+          if (!isOverflowing && isOpen) {
+            return true;
+          }
+
+          return isOverflowing;
+        });
+      }
+    };
+    checkOverflow();
+    const observer = new ResizeObserver(checkOverflow);
+    observer.observe(descriptionElement);
+  }, [text, lineClamp, isOpen]);
+
+  console.log("showReadMoreButton is", showReadMoreButton);
+  console.log("isOpen is", isOpen);
   return (
     <div className="description">
       <p
+        ref={descriptionRef}
         className={isOpen ? "expanded" : ""}
         style={{ WebkitLineClamp: lineClamp }}
       >
@@ -30,6 +61,19 @@ const ExpandableDescription = ({ text, lineClamp = 3 }) => {
           )}
         </div>
       )}
+      {/* <div onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <div className="expand-button">
+            <p>Read less</p>
+            <i className="fas fa-chevron-up"></i>
+          </div>
+        ) : (
+          <div className="expand-button">
+            <p>Read more</p>
+            <i className="fas fa-chevron-down"></i>
+          </div>
+        )}
+      </div> */}
     </div>
   );
 };
