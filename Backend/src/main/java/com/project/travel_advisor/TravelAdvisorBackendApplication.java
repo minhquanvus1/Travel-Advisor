7220c042ffee53ee4cc601e94eac2368a45da1cb
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-14 22:05:09 +0700

    set development db connection

--- Backend/src/main/java/com/project/travel_advisor/TravelAdvisorBackendApplication.java
+++ Backend/src/main/java/com/project/travel_advisor/TravelAdvisorBackendApplication.java
@@ -2,9 +2,8 @@ package com.project.travel_advisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TravelAdvisorBackendApplication {
 
 	public static void main(String[] args) {
