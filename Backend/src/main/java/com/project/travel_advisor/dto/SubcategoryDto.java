c00ef87054907b468899927ed2f3dc95bb24ae5c
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-17 11:31:49 +0700

    create SubcategoryDto, and SubcategoryMapper

---
+++ Backend/src/main/java/com/project/travel_advisor/dto/SubcategoryDto.java
@@ -0,0 +1,17 @@
+package com.project.travel_advisor.dto;
+
+import com.project.travel_advisor.entity.Attraction;
+import com.project.travel_advisor.entity.Tour;
+
+import java.util.List;
+
+public record SubcategoryDto(Long id,
+
+                             String name,
+
+                             Long categoryId,
+                             List<Attraction> attractions,
+                             List<Tour> tours) {
+
+
+}
