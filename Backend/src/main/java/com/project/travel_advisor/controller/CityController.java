e89c0d7ca620de498e3d43b7ab116d7ef5bdddde
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-17 18:25:51 +0700

    create endpoint for createACity(), deleteCityById(), and return CityDto for endpoint findAllCity(), and findCityById()

--- Backend/src/main/java/com/project/travel_advisor/controller/CityController.java
+++ Backend/src/main/java/com/project/travel_advisor/controller/CityController.java
@@ -1,14 +1,16 @@
 package com.project.travel_advisor.controller;
 
+import com.project.travel_advisor.dto.CityDto;
 import com.project.travel_advisor.entity.City;
 import com.project.travel_advisor.service.city.CityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +19,25 @@ public class CityController {
     private final CityService cityService;
 
     @GetMapping("/cities")
-    public ResponseEntity<List<City>> findAllCity() {
+    public ResponseEntity<List<CityDto>> findAllCity() {
         return ResponseEntity.ok(cityService.findAllCity());
     }
 
     @GetMapping("/cities/{id}")
-    public ResponseEntity<City> findCityById(@PathVariable Long id) {
+    public ResponseEntity<CityDto> findCityById(@PathVariable Long id) {
         return ResponseEntity.ok(cityService.findCityById(id));
+    }
+
+    @PostMapping("/cities")
+    public ResponseEntity<CityDto> createACity(@RequestBody CityDto cityDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(cityService.createACity(cityDto));
+    }
+
+    @DeleteMapping("/cities/{id}")
+    public ResponseEntity<Map<String, Object>> deleteCityById(@PathVariable Long id) {
+        cityService.deleteACityById(id);
+        Map<String, Object> response = new HashMap<>();
+        response.put("deletedId", id);
+        return ResponseEntity.ok(response);
     }
 }
