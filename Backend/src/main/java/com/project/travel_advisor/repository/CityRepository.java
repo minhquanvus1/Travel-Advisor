9a5fa55e6497c133d4f0e8668ea46cee66cc3270
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-17 18:23:29 +0700

    add a method to findCityByNameIgnoreCase()

--- Backend/src/main/java/com/project/travel_advisor/repository/CityRepository.java
+++ Backend/src/main/java/com/project/travel_advisor/repository/CityRepository.java
@@ -3,5 +3,9 @@ package com.project.travel_advisor.repository;
 import com.project.travel_advisor.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findCityByNameIgnoreCase(String name);
 }
