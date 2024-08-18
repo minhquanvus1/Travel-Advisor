9c30babb98f27978fbd12e848945f64c8256fb17
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-18 14:19:02 +0700

    create method to find entity ignore case

--- Backend/src/main/java/com/project/travel_advisor/repository/RestaurantRepository.java
+++ Backend/src/main/java/com/project/travel_advisor/repository/RestaurantRepository.java
@@ -3,5 +3,9 @@ package com.project.travel_advisor.repository;
 import com.project.travel_advisor.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    Optional<Restaurant> findRestaurantByNameIgnoreCase(String name);
 }
