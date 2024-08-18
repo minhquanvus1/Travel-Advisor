6dc7ce6b01291ad94811237dc422bf715f18631a
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-18 14:29:04 +0700

    create endpoint for POST /restaurants to create a Restaurant

--- Backend/src/main/java/com/project/travel_advisor/controller/RestaurantController.java
+++ Backend/src/main/java/com/project/travel_advisor/controller/RestaurantController.java
@@ -1,12 +1,12 @@
 package com.project.travel_advisor.controller;
 
+import com.project.travel_advisor.dto.RestaurantDto;
 import com.project.travel_advisor.entity.Restaurant;
 import com.project.travel_advisor.service.restaurant.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +17,17 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @GetMapping("/restaurants")
-    public ResponseEntity<List<Restaurant>> findAllRestaurant() {
+    public ResponseEntity<List<RestaurantDto>> findAllRestaurant() {
         return ResponseEntity.ok(restaurantService.findAllRestaurant());
     }
 
     @GetMapping("/restaurants/{id}")
-    public ResponseEntity<Restaurant> findRestaurantById(@PathVariable Long id) {
+    public ResponseEntity<RestaurantDto> findRestaurantById(@PathVariable Long id) {
         return ResponseEntity.ok(restaurantService.findRestaurantById(id));
+    }
+
+    @PostMapping("/restaurants")
+    public ResponseEntity<RestaurantDto> createARestaurant(@RequestBody RestaurantDto restaurantDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(restaurantService.createARestaurant(restaurantDto));
     }
 }
