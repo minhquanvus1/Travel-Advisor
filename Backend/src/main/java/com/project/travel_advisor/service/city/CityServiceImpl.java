799f70ed073e952e0072acca9d6d219a46128b81
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-21 16:58:12 +0700

    update the logic in createACity(), and deleteACityById() to set/unset TravelAdvice for the city

--- Backend/src/main/java/com/project/travel_advisor/service/city/CityServiceImpl.java
+++ Backend/src/main/java/com/project/travel_advisor/service/city/CityServiceImpl.java
@@ -2,11 +2,13 @@ package com.project.travel_advisor.service.city;
 
 import com.project.travel_advisor.dto.CityDto;
 import com.project.travel_advisor.entity.City;
+import com.project.travel_advisor.entity.TravelAdvice;
 import com.project.travel_advisor.exception.BadRequestException;
 import com.project.travel_advisor.exception.ResourceNotFoundException;
 import com.project.travel_advisor.mapper.CityMapper;
 import com.project.travel_advisor.repository.CityRepository;
 import com.project.travel_advisor.repository.CuisineRepository;
+import com.project.travel_advisor.repository.TravelAdviceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,7 @@ public class CityServiceImpl implements CityService{
 
     private final CityRepository cityRepository;
     private final CuisineRepository cuisineRepository;
+    private final TravelAdviceRepository travelAdviceRepository;
 
     @Override
     public List<CityDto> findAllCity() {
@@ -31,11 +34,19 @@ public class CityServiceImpl implements CityService{
     }
 
     @Override
+    @Transactional
     public CityDto createACity(CityDto cityDto) {
         City city = CityMapper.mapToCity(cityDto);
         System.out.println("City object is " + city.getCuisines());
         cityRepository.findCityByNameIgnoreCase(city.getName()).ifPresent((foundCity) -> {throw new BadRequestException("City with name " + city.getName() + " already exists");});
         city.getCuisines().forEach(cuisine -> cuisine.getCities().add(city));
+        city.getTravelAdvice().setCity(city);
+        TravelAdvice travelAdvice = city.getTravelAdvice();
+        travelAdvice.getGettingTheres().forEach(i -> i.setTravelAdvice(travelAdvice));
+        travelAdvice.getGettingArounds().forEach(i -> i.setTravelAdvice(travelAdvice));
+        travelAdvice.getOnTheGrounds().forEach(i -> i.setTravelAdvice(travelAdvice));
+        travelAdvice.getCustoms().forEach(i -> i.setTravelAdvice(travelAdvice));
+        travelAdviceRepository.save(travelAdvice);
         cityRepository.save(city);
         return CityMapper.mapToCityDto(city);
     }
@@ -54,6 +65,13 @@ public class CityServiceImpl implements CityService{
             }
         });
         foundCity.getCuisines().clear();
+        TravelAdvice travelAdvice = foundCity.getTravelAdvice();
+        travelAdvice.getGettingTheres().forEach(i -> i.setTravelAdvice(null));
+        travelAdvice.getGettingArounds().forEach(i -> i.setTravelAdvice(null));
+        travelAdvice.getOnTheGrounds().forEach(i -> i.setTravelAdvice(null));
+        travelAdvice.getCustoms().forEach(i -> i.setTravelAdvice(null));
+        travelAdvice.setCity(null);
+        foundCity.setTravelAdvice(null);
         cityRepository.delete(foundCity);
     }
 }
