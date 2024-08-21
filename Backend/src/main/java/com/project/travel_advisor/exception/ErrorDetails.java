a739e087dc6de6a812726ec9ab61a21f9b58ad9f
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-21 10:31:21 +0700

    add the "errors" field, and only include non-null fields in the JSON Response (Serialization) of ErrorDetails

--- Backend/src/main/java/com/project/travel_advisor/exception/ErrorDetails.java
+++ Backend/src/main/java/com/project/travel_advisor/exception/ErrorDetails.java
@@ -1,12 +1,17 @@
 package com.project.travel_advisor.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDetails {
 
     private Boolean success;
@@ -14,4 +19,6 @@ public class ErrorDetails {
     private Integer error;
 
     private String message;
+
+    private Map<String,String> errors = new HashMap<>();
 }
