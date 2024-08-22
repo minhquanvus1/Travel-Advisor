2568227c9d6684485de3b6bcfa9bd27bf6663db3
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-08-22 20:16:02 +0700

    create TourRepository, and LanguageRepository

---
+++ Backend/src/main/java/com/project/travel_advisor/repository/LanguageRepository.java
@@ -0,0 +1,7 @@
+package com.project.travel_advisor.repository;
+
+import com.project.travel_advisor.entity.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+}
