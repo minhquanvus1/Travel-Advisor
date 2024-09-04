502d4efae466049174cdbacacbbe969264372634
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-09-04 22:02:36 +0700

    use "cities" of CityContext, and call GET /attractions to get all attractions

--- Frontend/src/pages/ThingsToDo/ThingsToDo.jsx
+++ Frontend/src/pages/ThingsToDo/ThingsToDo.jsx
@@ -3,28 +3,37 @@ import "./ThingsToDo.css";
 import { CityContext } from "../../context/CityContextProvider";
 import TravelDestinationsHeader from "../../components/TravelDestinationsHeader/TravelDestinationsHeader";
 import TravelDestinationsService from "../../components/TravelDestinationsService/TravelDestinationsService";
-import { cities, attractions, subCategory } from "../../assets/assets";
+// import { cities, subCategory } from "../../assets/assets";
 import { Link } from "react-router-dom";
 import CityCard from "../../components/CityCard/CityCard";
 import { replaceWhiteSpaceWithUnderScore } from "../../functions/replaceWhiteSpaceWithUnderScore";
 import RatingStars from "../../components/RatingStars/RatingStars";
+import { axiosInstance } from "../../apis/axiosInstance";
+import { useAxios } from "../../hooks/useAxios";
 
 const ThingsToDo = () => {
-  const { setCityState } = useContext(CityContext);
+  const { cities, setCityState } = useContext(CityContext);
   useEffect(() => {
     setCityState("");
   }, []);
-  const findSubCategory = (attraction) => {
-    const foundSubCategory = subCategory.find(
-      (subCategory) => subCategory.id === attraction.subCategoryId
-    );
-    console.log("foundSubCategory is", foundSubCategory);
-    return foundSubCategory;
-  };
-  const findCityById = (cityId) => {
-    const foundCity = cities.find((city) => city.id === cityId);
-    return foundCity;
-  };
+
+  const [attractions, error, loading] = useAxios({
+    axiosInstance: axiosInstance,
+    url: "/attractions",
+    method: "GET",
+  });
+
+  // const findSubCategory = (attraction) => {
+  //   const foundSubCategory = subCategory.find(
+  //     (subCategory) => subCategory.id === attraction.subCategoryId
+  //   );
+  //   console.log("foundSubCategory is", foundSubCategory);
+  //   return foundSubCategory;
+  // };
+  // const findCityById = (cityId) => {
+  //   const foundCity = cities.find((city) => city.id === cityId);
+  //   return foundCity;
+  // };
 
   return (
     <div className="things-to-do-section">
@@ -35,6 +44,7 @@ const ThingsToDo = () => {
           <div className="top-destinations-container">
             <div className="title">Top destinations in Vietnam</div>
             <div className="restaurants-in-top-cities-list">
+              {cities.length <= 0 && "No cities found to find Top Destinations"}
               {cities.length > 0 &&
                 cities.slice(0, 4).map((city, index) => {
                   return (
@@ -55,15 +65,16 @@ const ThingsToDo = () => {
           <div className="top-destinations-container">
             <div className="title">Top attractions in Vietnam</div>
             <div className="restaurants-in-top-cities-list">
+              {error && "No attractions found"}
+              {loading && "Loading..."}
               {attractions.length > 0 &&
                 attractions.slice(0, 4).map((attraction, index) => {
-                  const foundCity = findCityById(attraction.cityId);
                   return (
                     <Link
                       to={`/cities/${replaceWhiteSpaceWithUnderScore(
-                        foundCity?.name
+                        attraction.cityName
                       )}/attractions/${replaceWhiteSpaceWithUnderScore(
-                        attraction.attractionName
+                        attraction.name
                       )}`}
                       key={attraction.id}
                     >
@@ -71,42 +82,12 @@ const ThingsToDo = () => {
                         <div className="image-container">
                           <img
                             src={attraction.imageUrl}
-                            alt={`${attraction.attractionName} image`}
+                            alt={`${attraction.name} image`}
                           />
                         </div>
                         <div className="card-contents">
-                          <div className="card-title">
-                            {attraction.attractionName}
-                          </div>
+                          <div className="card-title">{attraction.name}</div>
                           <div className="card-rating-count">
-                            {/* <svg
-                              viewBox="0 0 128 24"
-                              width="68"
-                              height="12"
-                              aria-labelledby=":lithium-Rlokd979qilt5vlq:"
-                            >
-                              <title id=":lithium-Rlokd979qilt5vlq:"></title>
-                              <path
-                                d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
-                                transform=""
-                              ></path>
-                              <path
-                                d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
-                                transform="translate(26 0)"
-                              ></path>
-                              <path
-                                d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
-                                transform="translate(52 0)"
-                              ></path>
-                              <path
-                                d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z"
-                                transform="translate(78 0)"
-                              ></path>
-                              <path
-                                d="M 12 0C5.389 0 0 5.389 0 12c0 6.62 5.389 12 12 12 6.62 0 12-5.379 12-12S18.621 0 12 0zm0 2a9.984 9.984 0 0110 10 9.976 9.976 0 01-10 10z"
-                                transform="translate(104 0)"
-                              ></path>
-                            </svg> */}
                             <RatingStars
                               rating={attraction.rating}
                               width={68}
@@ -123,7 +104,7 @@ const ThingsToDo = () => {
                             className="attraction-subcategory"
                             style={{ fontSize: "14px" }}
                           >
-                            {findSubCategory(attraction).subCategoryName}
+                            {attraction.subCategoryName}
                           </div>
                         </div>
                       </div>
