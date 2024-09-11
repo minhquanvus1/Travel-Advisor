7e50facbeaefbc45357031a3e861b9c4c1fcf394
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2024-09-11 09:55:59 +0700

    wrap the <App/> inside the UserContextProvider, and Auth0Provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,13 +4,29 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import CityContextProvider from "./context/CityContextProvider.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+import UserContextProvider from "./context/UserContextProvider.jsx";
+
+const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CityContextProvider>
-        <App />
-      </CityContextProvider>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: `${window.location.origin}`,
+          audience: "https://travel-advisor/api",
+        }}
+      >
+        <UserContextProvider>
+          <CityContextProvider>
+            <App />
+          </CityContextProvider>
+        </UserContextProvider>
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
